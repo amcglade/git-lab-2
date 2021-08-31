5f545bc4289631efb07b184c43c85e8b93d0913c
Alexander <am563918@ohio.edu>
2021-08-31 17:54:24 -0400

    program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Alexander MCGlade
+ *        Date: 8\31\2021
+ * Description: says hello
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
